072568cd0fdb5f224f10c26f7bc27587b4875069
wesenbergg <b.b.j.1998@gmail.com>
2019-10-27 15:31:13 +0200

    unicafe done, anekdootit 2 parts done

--- osa1-react-alkeet/unicafe/src/index.js
+++ osa1-react-alkeet/unicafe/src/index.js
@@ -1,6 +1,51 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = props => {
+    return(
+        <button onClick={() => props.setteri(props.value+1)}>{props.name}</button>
+    )
+}
+
+const Statistics = props => {
+    let kaikki = props.good+props.neutral+props.bad
+    let pos = props.good/kaikki
+
+    if(kaikki === 0) {
+        return (
+            <>
+                <h2>statistics</h2>
+                <p>No feedback given</p>
+            </>
+        )
+    }
+
+    return(
+        <>
+        <h2>statistics</h2>
+        <table>
+        <Statistic name="good" value={props.good}/>
+        <Statistic name="neutral" value={props.neutral}/>
+        <Statistic name="bad" value={props.bad}/>
+        <Statistic name="kaikki" value={props.kaikki}/>
+        <Statistic name="average" value={(props.good-props.bad)/kaikki}/>
+        <Statistic name="positive" value={pos}/>
+        </table>
+        </>
+    )
+}
+
+const Statistic = props => {
+    return(
+        <>
+        <tr>
+            <th>{props.name}</th>
+            <th>{props.value}</th>
+        </tr>
+        </>
+    )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,9 +53,14 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <h2>give feedback</h2>
+      <Button name="good" value={good} setteri={setGood}/>
+      <Button name="neutral" value={neutral} setteri={setNeutral}/>
+      <Button name="bad" value={bad} setteri={setBad}/>
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </>
   )
 }
 
