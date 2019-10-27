e909b5c01ffb2a26407affd48f36b0b944193ce4
wesenbergg <b.b.j.1998@gmail.com>
2019-10-27 13:39:58 +0200

    delete osa1

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Header = props => {
+    return(
+        <>
+            <h1>{props.course}</h1>
+        </>
+    )
+}
+
+const Content = props => {
+    return(
+        <>
+        <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+        <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+        <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
+        </>
+    )
+}
+
+const Total = props => {
+    return(
+        <>
+        <p>Number of exercises {props.sum}</p>
+        </>
+    )
+}
+
+const Part = props => {
+    return(
+        <>
+        <p>{props.part} {props.exercise}</p>
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
