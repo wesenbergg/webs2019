288d00ce51c1ef9b09c71a2e19fde7fae1f8f824
wesenbergg <b.b.j.1998@gmail.com>
2019-11-01 20:57:12 +0200

    osa2 valmis

--- osa2-palvelimen-kanssa-kommunikointi/puhelinluettelo/src/App.js
+++ osa2-palvelimen-kanssa-kommunikointi/puhelinluettelo/src/App.js
@@ -3,12 +3,14 @@ import personService from './services/persons'
 import Filter from './components/Filter.js'
 import AddPerson from './components/AddPerson.js'
 import Persons from './components/Persons.js'
+import Notification from './components/Notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ showFilter, setShowFilter ] = useState('')
+  const [ message, setMessage ] = useState(null)
 
   useEffect(() => {
     personService
@@ -23,9 +25,13 @@ const App = () => {
         .create(newObject)
         .then(returnedPerson => {
             setPersons(persons.concat(returnedPerson))
-        })
-        .catch(e =>{
-            alert("Oops something went wrong")
+            setMessage({
+              type: 'success',
+              message: `Succesfully added '${returnedPerson.name}' to server`
+            })
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
         })
   }
 
@@ -37,22 +43,47 @@ const App = () => {
     .then(returnedPerson => {
       //console.log(returnedP) => undefined
       setPersons(persons.map(person => person.id !== newObject.id ? person : newObject))
+      setMessage({
+        type: 'success',
+        message: `Succesfully updated ${newObject.name}'s number.`
+      })
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }).catch(error => {
+      setMessage({
+        type: 'error',
+        message: `Person '${newObject.name}' was already removed from server`
+      })
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+      setPersons(persons.filter(p => p.id !== newObject.id))
     })
   }
 
   const handleDelete = e => {
     let id = parseInt(e.target.value)
+    let foundPerson = persons.find(p => p.id === id)
 
     personService
     .deleteById(id)
-    .then(r =>
-        console.log(persons))
+    .then(r => {
+      setMessage({
+        type: 'success',
+        message: `Succesfully removed '${foundPerson.name}' from server`
+      })
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    })
     setPersons(persons.filter(p => p.id !== id))
 }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message}/>
       <Filter showFilter={showFilter} setShowFilter={setShowFilter}/>
       <AddPerson newName={newName} setNewName={setNewName}
        newNumber={newNumber} setNewNumber={setNewNumber} persons={persons}
